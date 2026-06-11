7bf9c159a07afae3ed67cda56b9ba6910aac6fc4
NehorayLahmi <xr329117@gmail.com>
2026-06-11 12:51:28 +0300

    שינוי שם פרוייקט

--- lib/config.ts
+++ lib/config.ts
@@ -5,5 +5,5 @@ export const BACKEND_URL =
         : process.env.BACKEND_URL!;
 
 export const SITE_URL = (
-  process.env.FRONTEND_URL ?? "https://leads-progect-next-js.vercel.app"
+  process.env.FRONTEND_URL ?? "https://leads-project-next-js.vercel.app"
 ).replace(/\/$/, "");
